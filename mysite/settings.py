140397e07f982a637daedd57bac91d47a42c1a48
harshitraj33 <harshitpriv.3@gmail.com>
2026-02-03 09:37:39 +0530

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ TAILWIND_APP_NAME = "theme"
 INSTALLED_APPS = [
     'tailwind',
     'theme',
-    'portfolio',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
