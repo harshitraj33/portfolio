37369845ca4d4677c4d0e4b05843f1c7a0eb4565
harshitraj33 <harshitpriv.3@gmail.com>
2026-02-05 09:45:40 +0530

    update

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,3 @@
-# from django.shortcuts import render
-# from django.template import loader
-# from django.http import HttpResponse
-# from .models import users
-
-# # def index(request):
-# #     myusers = users.objects.all().values()
-# #     template = loader.get_template('user_list.html')
-# #     context = {
-# #         'myusers' : myusers,
-# #     }
-# #     return HttpResponse(template,render(context, request))
-
-
-
 from django.http import HttpResponse
 
 def index(request):
